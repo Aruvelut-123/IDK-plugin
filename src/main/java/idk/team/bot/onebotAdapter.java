ae120911e2cf886462235f6dbc325454cf70e3dc
Aruvelut <2502474438@qq.com>
2024-08-15 20:27:38 +0800

    更新1.2.7

---
+++ src/main/java/idk/team/bot/onebotAdapter.java
@@ -0,0 +1,11 @@
+package idk.team.bot;
+
+public class onebotAdapter {
+    public void init() {
+        // TODO: Implement onebot adapter
+        // Download onebot libraries for java
+        // Load onebot libraries for java
+        // Start onebot server
+
+    }
+}
